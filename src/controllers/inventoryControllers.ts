9db711eca48263509c043e4c73437c522a8dd052
Charles Poulin <charlespoulin@Charless-MacBook-Pro.local>
2023-11-11 16:33:57 -0600

    Added set inventory controller

--- src/controllers/inventoryControllers.ts
+++ src/controllers/inventoryControllers.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-// const { ObjectId } = require("mongoose").Types; 
+const { ObjectId } = require("mongoose").Types; 
 const Inventory = require("../models/Inventory.js");
 
 const getInventory = async (req: Request, res: Response) => {
@@ -11,8 +11,22 @@ const getInventory = async (req: Request, res: Response) => {
     }
 }
 
-const setInventory = (req: Request, res: Response) => {
-    
+const setInventory = async (req: Request, res: Response) => {
+    try {
+        const inventoryId = ObjectId(req.body.id);
+
+        if (inventoryId) {
+            const inventory = await Inventory.updateOne(
+                {id: inventoryId},
+                req.body
+            );
+            res.status(200).json(inventory); 
+        } else {
+            res.status(400).send({ message: "Missing Inventory ID" });
+        }
+    } catch (error: any) {
+        res.status(500).json({message: error.message});
+    }
 }
 
 module.exports = { 
