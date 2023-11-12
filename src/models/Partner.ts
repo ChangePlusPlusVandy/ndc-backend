2165a05da79a5758dfc7e4a4c0960f9ff8b0d05f
m1chelle7 <120818513+m1chelle7@users.noreply.github.com>
2023-11-12 12:51:27 -0600

    Michelle/partner schema (#13)
    * Backend commit (partner)
    
    * Added partner schema
    
    ---------
    
    Co-authored-by: Michelle <michelle@Michelles-MacBook-Pro.local>
    Co-authored-by: Kai Dubauskas <kaidubauskas@gmail.com>

---
+++ src/models/Partner.ts
@@ -0,0 +1,68 @@
+import { ObjectId } from "mongodb";
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const PartnerSchema = new Schema ({
+    id: {
+        type: ObjectId,
+    },
+    orders: [{
+        type: Schema.Types.ObjectID,
+        ref: "Order",
+    }],
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: [
+            'COMMUNITY',
+            'CLINIC',
+            'DFD',
+            'SCHOOL',
+        ],
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    dateJoined: {
+        type: Date,
+    },
+    numOrdersMonth: {
+        type: Number,
+        required: true,
+    },
+    numOrdersYTD: {
+        type: Number,
+        required: true,
+    },
+    numOrdersTotal: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    deliveryInstructions: {
+        type: String,
+    },
+})
+
+module.exports = mongoose.model("Partner", PartnerSchema);
